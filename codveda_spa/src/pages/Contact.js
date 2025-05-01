9240f63f3dc8c2ee28959d30219123052ccba732
A.R.Asrin Jumana <145181474+AsrinJumana712@users.noreply.github.com>
2025-05-01 19:29:14 +0530

    Add contact details

--- codveda_spa/src/pages/Contact.js
+++ codveda_spa/src/pages/Contact.js
@@ -1,14 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Contact = () => {
   const { user } = useContext(AppContext);
 
-  return (
-    <div className='container'>
-      <h1>Contact Page</h1>
-      <p>{user}, you can reach us at contact@example.com</p>
-    </div>
+  return(
+      <div className="contact-info">
+        <h2>Our Contact Information</h2>
+        <p>Email: contact@homeyeats.com</p>
+        <p>Phone: +94 77 123 4567</p>
+        <p>Location: Colombo, Sri Lanka</p>
+      </div>
   );
 };
 
